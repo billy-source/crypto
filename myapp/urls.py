eff98193d8998d8184d458b04f0ce4ab7a82f843
kbilly <kemboibilly49@gmail.com>
2025-08-30 23:15:54 +0300

    update the urls in myproject

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('wallet/<int:user_id>/', views.wallet_view, name='wallet'),
+
+    # Later you’ll add these:
+    # path('holdings/<int:user_id>/', views.holdings_view, name='holdings'),
+    # path('trades/<int:user_id>/', views.trades_view, name='trades'),
 ]
